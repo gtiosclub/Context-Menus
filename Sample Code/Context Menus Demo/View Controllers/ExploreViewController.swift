69ac007317a01aeb7b1614976210a8bae9da3a42
Cal Stephens <cal.stephens@airbnb.com>
2019-10-03 15:16:39 -0700

    Add a Context Menu on the Explore screen

--- Sample Code/Context Menus Demo/View Controllers/ExploreViewController.swift
+++ Sample Code/Context Menus Demo/View Controllers/ExploreViewController.swift
@@ -94,3 +94,121 @@ extension ExploreViewController: UICollectionViewDelegateFlowLayout {
   }
 
 }
+
+// MARK: ExploreViewController + Context Menus
+
+/// These methods come from `UICollectionViewDelegate`
+extension ExploreViewController {
+
+  /// `contextMenuConfigurationForItemAt` is called when the user long-presses one of the `UICollectionView` cells
+  /// In this method, you set up your Context Menu.
+  func collectionView(
+    _ collectionView: UICollectionView,
+    contextMenuConfigurationForItemAt indexPath: IndexPath,
+    point: CGPoint) -> UIContextMenuConfiguration?
+  {
+    let listing = listings[indexPath.item]
+
+    /// Use the listing's ID as the identifier for the Context Menu.
+    /// We have to cast the ID String to `NSString`, because
+    /// the identifier object has to conform to the Objective-C Foundation `NSCopying` protocol.
+    return UIContextMenuConfiguration(
+      identifier: listing.id as NSString,
+      previewProvider: { () -> UIViewController? in
+        return nil
+      },
+      actionProvider: { _ in
+
+        /// The `actionProvider` block returns a `UIMenu` with a list of `UIAction`s.
+        /// These are the actions available in the Context Menu.
+
+        let favoriteAction: UIAction
+        if !listing.isFavorite {
+          favoriteAction = UIAction(
+            title: "Favorite",
+            image: UIImage(systemName: "heart")!,
+            handler: { _ in
+              listing.isFavorite = true
+
+              // Reload the collection view once the animation finishes
+              DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                collectionView.reloadData()
+              })
+          })
+        } else {
+          favoriteAction = UIAction(
+            title: "Unfavorite",
+            image: UIImage(systemName: "heart.slash.fill")!,
+            handler: { _ in
+              listing.isFavorite = false
+
+              // Reload the collection view once the animation finishes
+              DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                collectionView.reloadData()
+              })
+          })
+        }
+
+        let shareOption = UIAction(
+          title: "Share",
+          image: UIImage(systemName: "square.and.arrow.up"),
+          handler: { _ in
+            let activityViewController = UIActivityViewController(
+              activityItems: ["Check out this listing on Airbnb: \(listing.title)"],
+              applicationActivities: nil)
+            
+            self.present(activityViewController, animated: true)
+        })
+
+        return UIMenu(
+          title: "",
+          children: [favoriteAction, shareOption])
+    })
+  }
+
+  /// `previewForHighlightingContextMenuWithConfiguration` decides
+  /// what part of the original view is used as the Context Menu preview
+  func collectionView(
+    _ collectionView: UICollectionView,
+    previewForHighlightingContextMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview?
+  {
+    /// Use the `configuration`'s `identifier` to find the corresponding `ListingCollectionViewCell`
+    guard let listingID = configuration.identifier as? String,
+      let listingIndex = listings.firstIndex(where: { $0.id == listingID }),
+      let listingCell = collectionView.cellForItem(at: IndexPath(item: listingIndex, section: 0)) as? ListingCollectionViewCell else
+    {
+      return nil
+    }
+
+    return UITargetedPreview(view: listingCell.imageContainer)
+  }
+
+  /// `previewForDismissingContextMenuWithConfiguration` is usually
+  /// the same as `previewForHighlightingContextMenuWithConfiguration`
+  func collectionView(
+    _ collectionView: UICollectionView,
+    previewForDismissingContextMenuWithConfiguration configuration: UIContextMenuConfiguration) -> UITargetedPreview?
+  {
+    return self.collectionView(collectionView, previewForHighlightingContextMenuWithConfiguration: configuration)
+  }
+
+  /// `willPerformPreviewActionForMenu` is called when the user taps on the Context Menu's preview view
+  /// Usually this can perform the same action as selecting the collection view cell
+  func collectionView(
+    _ collectionView: UICollectionView,
+    willPerformPreviewActionForMenuWith configuration: UIContextMenuConfiguration,
+    animator: UIContextMenuInteractionCommitAnimating)
+  {
+    /// Use the `configuration`'s `identifier` to find the corresponding `ListingCollectionViewCell`
+    guard let listingID = configuration.identifier as? String,
+      let listingIndex = listings.firstIndex(where: { $0.id == listingID }) else
+    {
+      return
+    }
+
+    animator.addCompletion {
+      self.collectionView(collectionView, didSelectItemAt: IndexPath(item: listingIndex, section: 0))
+    }
+  }
+
+}
